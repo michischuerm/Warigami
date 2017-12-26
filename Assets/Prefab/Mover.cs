8c2576af8cf1d6ca34c610f1d976d5051dcb3252
Jony Margelist <jony.margelist@stud.hslu.ch>
2017-12-26 20:12:16 +0100

    Changed Mover.cs Script
    added pitchRange, changed random from complete random to pseudo random

--- Assets/Prefab/Mover.cs
+++ Assets/Prefab/Mover.cs
@@ -1,31 +1,58 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Mover : MonoBehaviour
-{
-	public float speed;
-	public float lifetime;
-	public float upScale;
+public class Mover : MonoBehaviour {
+    public float speed;
+    public float lifetime;
+    public float upScale;
+
+
 
     public AudioClip[] shootSounds;
-    private AudioSource audioSource;
 
-	void Start ()
-	{
-		Rigidbody rb = (Rigidbody)GetComponent(typeof(Rigidbody));
-		rb.velocity =  transform.forward * speed + transform.up * upScale;
+    [Range(0, 36f)]//Value below
+    public float pitchRange = -36f;
+
+    public float minVolumeRange = 0.8f;
+    public float maxVolumeRange = 1f;
+
+
+    private AudioSource audioSource;
+    private int randomValueFromSoundArray;
+    private static int previousRandomValue = 0;
+    private float pitchOctaveValue = 1.05946f;
+
+
+
+    void Start() {
+        Rigidbody rb = (Rigidbody)GetComponent(typeof(Rigidbody));
+        rb.velocity = transform.forward * speed + transform.up * upScale;
 
         audioSource = gameObject.GetComponent<AudioSource>();
+        disableRandomAudioSuccessively();
         playRandomSoundOnStart();
-	}
+
+    }
+
+    void Awake() {
+        Destroy(gameObject, lifetime);
+    }
 
     void playRandomSoundOnStart() {
-        audioSource.clip = shootSounds[Random.Range(0, shootSounds.Length)];
+        audioSource.clip = shootSounds[randomValueFromSoundArray];
+
+
+        audioSource.pitch = Mathf.Pow(pitchOctaveValue, (1 + (Random.Range(-pitchRange, +pitchRange))));
+
+        audioSource.volume = Random.Range(minVolumeRange, maxVolumeRange);
         audioSource.Play();
     }
 
-    void  Awake ()
-	{
-		Destroy(gameObject, lifetime);
-	}
+    private void disableRandomAudioSuccessively() {
+        do {
+            randomValueFromSoundArray = Random.Range(0, shootSounds.Length);
+        }
+        while (previousRandomValue == randomValueFromSoundArray);
+        previousRandomValue = randomValueFromSoundArray;
+    }
 }
