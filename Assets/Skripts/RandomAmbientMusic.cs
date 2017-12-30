c31ea521c027fdf5677c6ab070294a68c4c974bc
Jony Margelist <jony.margelist@stud.hslu.ch>
2017-12-30 19:02:59 +0100

    Added Script for Random Ambient Music
    Its on the camera. Better|Cleaner way to do this?

---
+++ Assets/Skripts/RandomAmbientMusic.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomAmbientMusic : MonoBehaviour {
+
+
+    public AudioClip[] ambientSounds;
+
+    private AudioSource audioSource;
+    private int randomValueFromSoundArray;
+    private static int previousRandomValue = 0;
+
+
+    // Use this for initialization
+    void Start() {
+        audioSource = gameObject.GetComponent<AudioSource>();
+        playRandomSoundOnStart();
+
+    }
+
+
+    void playRandomSoundOnStart() {
+        audioSource.clip = ambientSounds[randomValueFromSoundArray];
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
