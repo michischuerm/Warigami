a5694be9910cc7ed2e6ef592f1d7435b69ac6b79
Christian Mammoliti <christian.mammoliti@outlook.com>
2018-01-03 18:34:17 +0100

    UI Controls und Credits ergänzt
    image vo controls muess no apasst wärde ufs nöie mit rotiere...aber ich pushes mol to be safe

--- Assets/Skripts/UI.cs
+++ Assets/Skripts/UI.cs
@@ -8,29 +8,69 @@ public class UI : MonoBehaviour {
 	public Canvas quitMenu;
 	public Button startText;
 	public Button exitText;
+	public Button creditsText;
+	public Canvas creditsMenu;
+	public Canvas controlsMenu;
+	public Button controlsText;
 		
 	
 	// Use this for initialization
 	void Start () {
 
 		quitMenu = quitMenu.GetComponent<Canvas> ();
+		creditsMenu = creditsMenu.GetComponent<Canvas> ();
+		controlsMenu = controlsMenu.GetComponent<Canvas> ();
 		startText = startText.GetComponent<Button> ();
 		exitText = exitText.GetComponent<Button> ();
+		creditsText = creditsText.GetComponent<Button> ();
 		quitMenu.enabled = false;
+		creditsMenu.enabled = false;
+		controlsMenu.enabled = false;
 	}
 
 	public void ExitPress ()
 	{
 		quitMenu.enabled = true;
+		creditsMenu.enabled = false;
 		startText.enabled = false;
 		exitText.enabled = false;
+		creditsText.enabled = false;
+		controlsText.enabled = false;
+		controlsMenu.enabled = false;
 	}
+
+	public void CreditsPress ()
+	{
+		quitMenu.enabled = false;
+		creditsMenu.enabled = true;
+		startText.enabled = false;
+		exitText.enabled = false;
+		creditsText.enabled = false;
+		controlsText.enabled = false;
+		controlsMenu.enabled = false;
+	}
+
+	public void ControlsPress ()
+	{
+		quitMenu.enabled = false;
+		creditsMenu.enabled = false;
+		startText.enabled = false;
+		exitText.enabled = false;
+		creditsText.enabled = false;
+		controlsText.enabled = false;
+		controlsMenu.enabled = true;
+	}
+
 
 	public void noPress()
 	{
 		quitMenu.enabled = false;
 		startText.enabled = true;
 		exitText.enabled = true;
+		creditsText.enabled = true;
+		creditsMenu.enabled = false;
+		controlsText.enabled = true;
+		controlsMenu.enabled = false;
 	}
 
 	public void StartLevel ()
